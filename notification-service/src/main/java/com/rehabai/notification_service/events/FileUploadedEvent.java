4cf8c492db0014d62ce48be8abb35abbcf70591c
henriquecpp <henriquedotcpp@gmail.com>
2025-10-27 15:42:15 -0300

    feat: Adicionado evento FileUploaded para notificação de uploads de arquivos

---
+++ notification-service/src/main/java/com/rehabai/notification_service/events/FileUploadedEvent.java
@@ -0,0 +1,13 @@
+package com.rehabai.notification_service.events;
+
+import java.util.UUID;
+
+public record FileUploadedEvent(
+        UUID id,
+        String bucket,
+        String s3Path,
+        String originalName,
+        long sizeBytes,
+        String hashSha256
+) {}
+
