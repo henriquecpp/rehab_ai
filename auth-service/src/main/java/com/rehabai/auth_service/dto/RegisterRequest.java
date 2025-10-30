014d475fbfcea49e8d2feea1e915877cc732a16f
henriquecpp <henriquedotcpp@gmail.com>
2025-10-30 17:26:29 -0300

    refactor: removido campo de nome de usuário do DTO RegisterRequest

--- auth-service/src/main/java/com/rehabai/auth_service/dto/RegisterRequest.java
+++ auth-service/src/main/java/com/rehabai/auth_service/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.rehabai.auth_service.dto;
 
+import com.rehabai.auth_service.model.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,16 +11,16 @@ import jakarta.validation.constraints.Size;
  */
 public record RegisterRequest(
 
-        @NotBlank(message = "O nome de usuário é obrigatório")
-        @Size(min = 3, max = 100, message = "O nome de usuário deve ter entre 3 e 100 caracteres")
-        String username,
-
         @NotBlank(message = "O e-mail é obrigatório")
         @Email(message = "O formato do e-mail é inválido")
         String email,
 
         @NotBlank(message = "A senha é obrigatória")
         @Size(min = 6, max = 128, message = "A senha deve ter entre 6 e 128 caracteres")
-        String password
+        String password,
+
+        String fullName,
+
+        UserRole role
 ) {
 }
