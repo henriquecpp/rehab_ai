15ded61bdd4c0105ce8969a5ba95b8d1f7fb8e54
henriquecpp <henriquedotcpp@gmail.com>
2025-11-01 00:31:54 -0300

    feat: configuração inicial do AppConfig para gerenciamento de ObjectMapper e RestTemplate

---
+++ plan-service/src/main/java/com/rehabai/plan_service/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.rehabai.plan_service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory f = new SimpleClientHttpRequestFactory();
+        f.setConnectTimeout(5000);
+        f.setReadTimeout(5000);
+        return new RestTemplate(f);
+    }
+}
