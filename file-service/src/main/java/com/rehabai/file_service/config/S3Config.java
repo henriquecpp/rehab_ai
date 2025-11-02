18f29d826812c58ff7e0ae7469537f7584e20ed9
henriquecpp <henriquedotcpp@gmail.com>
2025-11-02 18:34:40 -0300

    refactor: atualização da configuração do cliente S3 para usar as novas variáveis de ambiente e cliente HTTP

--- file-service/src/main/java/com/rehabai/file_service/config/S3Config.java
+++ file-service/src/main/java/com/rehabai/file_service/config/S3Config.java
@@ -7,6 +7,8 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 
 import java.net.URI;
 
@@ -14,14 +16,18 @@ import java.net.URI;
 public class S3Config {
 
     @Bean
-    public S3Client s3Client(@Value("${AWS_ACCESS_KEY_ID}") String accessKey,
-                             @Value("${AWS_SECRET_ACCESS_KEY}") String secretKey,
+    public S3Client s3Client(@Value("${S3_ACCESS_KEY_ID:minioadmin}") String accessKey,
+                             @Value("${S3_SECRET_ACCESS_KEY:minioadmin}") String secretKey,
                              @Value("${AWS_REGION:us-east-1}") String region,
                              @Value("${S3_ENDPOINT:}") String endpoint) {
         var creds = StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+        var s3conf = S3Configuration.builder().pathStyleAccessEnabled(true).build();
+        var http = UrlConnectionHttpClient.builder().build();
         var builder = S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(creds);
+                .credentialsProvider(creds)
+                .serviceConfiguration(s3conf)
+                .httpClient(http);
         if (endpoint != null && !endpoint.isBlank()) {
             builder = builder.endpointOverride(URI.create(endpoint));
         }
