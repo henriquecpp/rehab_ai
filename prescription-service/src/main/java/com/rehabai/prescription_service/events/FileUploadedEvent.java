8ebca94723154c3015c5ce13d130f13e4a78513f
henriquecpp <henriquedotcpp@gmail.com>
2025-11-11 22:37:39 -0300

    feat: adição do campo userId ao evento FileUploadedEvent

--- prescription-service/src/main/java/com/rehabai/prescription_service/events/FileUploadedEvent.java
+++ prescription-service/src/main/java/com/rehabai/prescription_service/events/FileUploadedEvent.java
@@ -4,10 +4,12 @@ import java.util.UUID;
 
 public record FileUploadedEvent(
         UUID id,
+        UUID userId,  // ⭐ Adicionado
         String bucket,
         String s3Path,
         String originalName,
         long sizeBytes,
-        String hashSha256
+        String hashSha256,
+        FileType fileType
 ) {}
 
