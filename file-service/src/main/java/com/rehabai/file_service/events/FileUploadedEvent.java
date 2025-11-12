41416cd73129f9899b1cd4985565532e383064e6
henriquecpp <henriquedotcpp@gmail.com>
2025-11-11 22:14:27 -0300

    feat: adição do campo userId e fileType no evento FileUploadedEvent

--- file-service/src/main/java/com/rehabai/file_service/events/FileUploadedEvent.java
+++ file-service/src/main/java/com/rehabai/file_service/events/FileUploadedEvent.java
@@ -1,12 +1,15 @@
 package com.rehabai.file_service.events;
 
+import com.rehabai.file_service.model.FileType;
 import java.util.UUID;
 
 public record FileUploadedEvent(
         UUID id,
+        UUID userId,  // ⭐ Adicionado
         String bucket,
         String s3Path,
         String originalName,
         long sizeBytes,
-        String hashSha256
+        String hashSha256,
+        FileType fileType
 ) {}
