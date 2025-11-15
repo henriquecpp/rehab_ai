ff281925cc576ec9af0bd9c14693a2e32f9c5e34
henriquecpp <henriquedotcpp@gmail.com>
2025-11-15 20:52:52 -0300

    feat: criação dos layouts default.vue e login.vue com estrutura básica

---
+++ front-end/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex min-h-screen">
+    <AppSidebar />
+    
+    <main class="flex-1 bg-gray-50" style="margin-left: 280px;">
+      
+      <AppHeader />
+      
+      <div class="p-8">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Este layout não precisa de script, mas se você precisar
+// buscar dados que valem para todo o layout, pode fazer aqui.
+</script>
---
+++ front-end/layouts/login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center bg-gradient-to-br from-[#667eea] to-[#764ba2]">
+    <slot />
+  </div>
+</template>
