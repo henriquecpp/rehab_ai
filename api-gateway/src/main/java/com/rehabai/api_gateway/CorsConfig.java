e8826d0c3ad9e717a7e226c1818d2833f4e9ea26
henriquecpp <henriquedotcpp@gmail.com>
2025-11-21 11:59:19 -0300

    feat: implementação da configuração CORS para permitir requisições do front-end (http://localhost:3001)

---
+++ api-gateway/src/main/java/com/rehabai/api_gateway/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.rehabai.api_gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:3001"));
+
+        config.setAllowedMethods(List.of("*"));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
