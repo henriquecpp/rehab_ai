06a8d222f66ee5ae29d0d6d558d764cc71b447e6
henriquecpp <henriquedotcpp@gmail.com>
2025-11-21 13:32:37 -0300

    feat: registros de filas para eventos/notificacoes de plano aprovado

--- notification-service/src/main/java/com/rehabai/notification_service/amqp/AmqpConfig.java
+++ notification-service/src/main/java/com/rehabai/notification_service/amqp/AmqpConfig.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableRabbit
 public class AmqpConfig {
+
 
     @Bean
     public DirectExchange fileEventsExchange(@Value("${amqp.fileExchange:file.events}") String name) {
@@ -29,6 +31,22 @@ public class AmqpConfig {
     public Binding notificationBinding(Queue notificationFileUploadedQueue, DirectExchange fileEventsExchange,
                                        @Value("${amqp.routingKeyUploaded:file.uploaded}") String routingKey) {
         return BindingBuilder.bind(notificationFileUploadedQueue).to(fileEventsExchange).with(routingKey);
+    }
+
+    @Bean
+    public TopicExchange planEventsExchange(@Value("${amqp.planExchange:plan.events}") String name) {
+        return new TopicExchange(name, true, false);
+    }
+
+    @Bean
+    public Queue planApprovedQueue(@Value("${amqp.planApprovedQueue:notification.plan.approved}") String q) {
+        return new Queue(q, true);
+    }
+
+    @Bean
+    public Binding planApprovedBinding(Queue planApprovedQueue, TopicExchange planEventsExchange,
+                                       @Value("${amqp.planApprovedRoutingKey:plan.approved}") String routingKey) {
+        return BindingBuilder.bind(planApprovedQueue).to(planEventsExchange).with(routingKey);
     }
 
     @Bean
