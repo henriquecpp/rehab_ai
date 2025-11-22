ca8c8bb702533f74bdd96c4d4900ae7731cc8fcf
henriquecpp <henriquedotcpp@gmail.com>
2025-11-22 12:46:30 -0300

    fix: permitindo o acesso publico da documentaçao pelo api gateway

--- auth-service/src/main/java/com/rehabai/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/rehabai/auth_service/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/auth/register", "/auth/login", "/auth/refresh", "/h2-console/**", "/actuator/**").permitAll()
+                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                     .anyRequest().authenticated()
             )
             .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
