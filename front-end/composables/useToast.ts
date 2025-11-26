b264a7d6a49dde3ef0bac85b7beeb512082b64f1
henriquecpp <henriquedotcpp@gmail.com>
2025-11-25 23:40:16 -0300

    feat: composable useToast para gerenciamento de notificações

---
+++ front-end/composables/useToast.ts
@@ -0,0 +1,42 @@
+import { ref } from "vue";
+
+interface Toast {
+  id: number;
+  message: string;
+  type: "success" | "error" | "info" | "warning";
+  duration?: number;
+}
+
+const toasts = ref<Toast[]>([]);
+let nextId = 1;
+
+export const useToast = () => {
+  const addToast = (
+    message: string,
+    type: Toast["type"] = "info",
+    duration = 3000
+  ) => {
+    const id = nextId++;
+    const toast: Toast = { id, message, type, duration };
+    toasts.value.push(toast);
+
+    if (duration > 0) {
+      setTimeout(() => {
+        removeToast(id);
+      }, duration);
+    }
+  };
+
+  const removeToast = (id: number) => {
+    const index = toasts.value.findIndex((t) => t.id === id);
+    if (index !== -1) {
+      toasts.value.splice(index, 1);
+    }
+  };
+
+  return {
+    toasts,
+    addToast,
+    removeToast,
+  };
+};
