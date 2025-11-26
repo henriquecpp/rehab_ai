1dee6f2833dd5d40edbcf69e31f35ae0073c00d2
henriquecpp <henriquedotcpp@gmail.com>
2025-11-25 23:40:34 -0300

    feat: adicionado UIToastContainer ao layout padrão

--- front-end/layouts/default.vue
+++ front-end/layouts/default.vue
@@ -2,17 +2,16 @@
 <template>
   <div class="flex min-h-screen">
     <AppSidebar />
-    
+
     <main class="flex-1 bg-gray-50 ml-72">
-      
       <AppHeader />
-      
+
       <div class="p-8">
         <slot />
       </div>
     </main>
+    <UIToastContainer />
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
